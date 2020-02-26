00d751114af12ae7a623898734a84cf8414015c3
ONoor <Onik.Noor@eviivo.com>
2020-02-26 19:28:44 +0000

    Main class now support clean environment

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ from cleanApp import cleanApp
 v = Validator()
 user = AutomicLogin("onoor", "OnikNoor")
 
-def cleanApplication(app, deploying):
+def cleanApplication(app, deploying): # Tested (/)
     ca = cleanApp(user)
     id = ca.get_application_id(user,app)
     profiles = ca.getProfiles(user, id)
@@ -22,4 +22,24 @@ def cleanApplication(app, deploying):
         else:
             print("Not deploying on " + profile)
 
-cleanApplication("AirbnbListingFeed", "4.10.1.108")
+#cleanApplication("AirbnbListingFeed", "4.10.1.108")
+
+def cleanEnvironment(env):
+    ce = cleanEnv(user)
+    apps = ce.get_versions(env)
+    for app in apps:
+
+        # app[0] is application name
+        # app[1] is PROD version
+        # app[2] is env version
+
+        checker = v.overrideBranch(app[1], app[2])
+        if(checker):
+            appId = ce.get_application_id(user, app[0])
+            profile = ce.getProfile(user, appId, env)
+            #print("Deploy for " + app[0])
+            ce.post_deployment(user, app, app[1], profile)
+        else:
+            print("Not deploying " + app[0])
+
+cleanEnvironment("STAGE1")
